db1794b9b4812121421a07266c99181f8498c7c6
nafishasalsabil <nafishasalsabil16@gmail.com>
2020-03-22 18:10:28 +0600

    The project has been modified

--- app/src/main/java/com/example/miniproject/Main3Activity.java
+++ app/src/main/java/com/example/miniproject/Main3Activity.java
@@ -7,9 +7,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
 
 public class Main3Activity extends AppCompatActivity implements View.OnClickListener {
     private Button button;
@@ -17,6 +21,7 @@ public class Main3Activity extends AppCompatActivity implements View.OnClickList
     private TextView questionNo,count,score;
     int pos = 0;
     int c = 0;
+    private ProgressBar progressBar;
     ArrayList<QuestionModel>questionModelArrayList;
 
     @Override
@@ -29,6 +34,7 @@ public class Main3Activity extends AppCompatActivity implements View.OnClickList
         questionNo = findViewById(R.id.questionno);
         count = findViewById(R.id.count);
         score = findViewById(R.id.score);
+        progressBar= findViewById(R.id.progressBar2);
         questionModelArrayList = new ArrayList<>();
         setupQ();
         setData();
@@ -47,28 +53,52 @@ public class Main3Activity extends AppCompatActivity implements View.OnClickList
         if(answer.equalsIgnoreCase(questionModelArrayList.get(pos).getAnswer()))
         {
             c++;
-            pos++;
-            setData();
-            editText.setText("");
+            new SweetAlertDialog(Main3Activity.this,SweetAlertDialog.SUCCESS_TYPE)
+                    .setTitleText("Good Job!")
+                    .setContentText("Right Answer")
+                    .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                        @Override
+                        public void onClick(SweetAlertDialog sweetAlertDialog) {
+                            pos++;
+                            setData();
+                            editText.setText("");
+                            sweetAlertDialog.dismiss();
+                        }
+                    })
+                    .show();
+
+
         }
         else
         {
-            pos++;
-            setData();
-            editText.setText("");
+            new SweetAlertDialog(Main3Activity.this,SweetAlertDialog.ERROR_TYPE)
+                    .setTitleText("Wrong Answer")
+                    .setConfirmText("OK")
+                    .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                        @Override
+                        public void onClick(SweetAlertDialog sweetAlertDialog) {
+                            sweetAlertDialog.dismiss();
+                            pos++;
+                            setData();
+                            editText.setText("");
+
+                        }
+                    })
+                    .show();
+
         }
-        if(pos==5)
-        {
-            openActivity4();
-        }
+
+
+        int x = ((pos+1)*100)/questionModelArrayList.size();
+        progressBar.setProgress(x);
     }
     public void setupQ()
     {
-        questionModelArrayList.add(new QuestionModel("what is 1+2?","3"));
+        questionModelArrayList.add(new QuestionModel("what is (10*2)/5?","4"));
         questionModelArrayList.add(new QuestionModel("what is 10*3?","30"));
         questionModelArrayList.add(new QuestionModel("what is 20/5?","4"));
-        questionModelArrayList.add(new QuestionModel("what is 10-7?","3"));
-        questionModelArrayList.add(new QuestionModel("what is 17+6?","23"));
+        questionModelArrayList.add(new QuestionModel("what is (10-7)/3?","1"));
+        questionModelArrayList.add(new QuestionModel("what is 17*6?","102"));
     }
     public void setData()
     {
@@ -78,8 +108,40 @@ public class Main3Activity extends AppCompatActivity implements View.OnClickList
             questionNo.setText("Question No: "+ (pos+1));
             score.setText("Score : " + c + "/" + questionModelArrayList.size());
         }
+        else
+        {
+            new SweetAlertDialog(Main3Activity.this,SweetAlertDialog.SUCCESS_TYPE)
+                    .setTitleText("Quiz Completed!")
+                    .setConfirmText("Restart")
+                    .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                        @Override
+                        public void onClick(SweetAlertDialog sDialog) {
+
+                            sDialog.dismissWithAnimation();
+                            pos = 0;
+                            c = 0;
+                            progressBar.setProgress(0);
+                            setData();
+                        }
+                    })
+                    .setCancelText("See Answers")
+                    .setCancelClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                        @Override
+                        public void onClick(SweetAlertDialog sDialog) {
+
+                            sDialog.dismissWithAnimation();
+                            openActivity4();
+
+                        }
+                    })
+                    .show();
+
+        }
+
 
     }
+
+
     private void openActivity4() {
         Intent intent = new Intent(this,Main4Activity.class);
         startActivity(intent);
--- app/src/main/java/com/example/miniproject/Main4Activity.java
+++ app/src/main/java/com/example/miniproject/Main4Activity.java
@@ -2,13 +2,17 @@ package com.example.miniproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Main4Activity extends AppCompatActivity implements View.OnClickListener {
 
     private Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,9 +22,18 @@ public class Main4Activity extends AppCompatActivity implements View.OnClickList
         button.setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View v) {
 
-        finish();
+
+
+    public void onClick(View v) {
+       // Toast.makeText(this, "Working", Toast.LENGTH_SHORT).show();
+
+       // System.exit(0);
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+
+
     }
+
+
 }
